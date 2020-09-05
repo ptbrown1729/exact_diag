f8faa8f28b1703a50caee121a11a6fd666f2ef87
Peter Brown <ptbrown1729@gmail.com>
2020-09-05 10:59:24 -0700

    removed unused scripts

--- scripts/test_hubbard_constructions.py
+++
@@ -1,8 +0,0 @@
-import ed_geometry as gm
-import ed_symmetry as sym
-import ed_fermions as hub
-
-geom = gm.Geometry.createSquareGeometry(2, 2, 0, 0, 0, 0)
-h = hub.fermions(geom, 0, 1, 1)
-ham = h.createH()
-

d7da343165d5d1c4eaa515b4d34d6f28af69b12f
Peter Brown <ptbrown1729@gmail.com>
2020-09-09 22:30:01 -0700

    added spatial inversion symmetry to heisenberg chain (where appropriate)

--- scripts/heisenberg_chain.py
+++ scripts/heisenberg_chain.py
@@ -9,13 +9,6 @@ import ed_symmetry as symm
 
 # set up system
 J = 1
-temps = np.array([0, 0.5, 1, 2, 10]) * J
-betas = np.divide(1, temps)
-betas[temps == 0] = np.inf
-
-# ###############################
-# constant number
-# ###############################
 gm = geom.Geometry.createSquareGeometry(10, 1, 0, 0, bc1_open=False, bc2_open=True)
 ss = spins.spinSystem(gm, jx=J, jy=J, jz=J)
 
@@ -28,6 +21,12 @@ xtransl_fn = symm.getTranslFn(np.array([[1], [0]]))
 xtransl_cycles, ntranslations = symm.findSiteCycles(xtransl_fn, ss.geometry)
 tx_op = ss.get_xform_op(xtransl_cycles)
 
+# inversion op
+cx, cy = ss.geometry.get_center_of_mass()
+inv_fn = symm.getInversionFn(cx, cy)
+inv_cycles, _ = symm.findSiteCycles(inv_fn, ss.geometry)
+inv_op = ss.get_xform_op(inv_cycles)
+
 # get projectors on mz subspace
 mz_projs, mzs = ss.get_subspace_projs(sz_op.tocsr())
 
@@ -35,7 +34,8 @@ mz_projs, mzs = ss.get_subspace_projs(sz_op.tocsr())
 projs = []
 mzs_all = []
 kxs_all = []
-parity_all = []
+spin_parity_all = []
+space_parity_all = []
 for p, mz in zip(mz_projs, mzs):
     tx_op_sub = p * tx_op * p.conj().transpose()
     tx_projs, kxs = symm.getZnProjectors(tx_op_sub, ss.geometry.nsites)
@@ -43,25 +43,55 @@ for p, mz in zip(mz_projs, mzs):
     if mz == 0:
         for tp, kx in zip(tx_projs, kxs):
             sz_flip_op_sub = tp * p * sz_flip_op * p.conj().transpose() * tp.conj().transpose()
-            sz_projs, parities = symm.getZnProjectors(sz_flip_op_sub, 2)
-            parities = np.round(np.exp(1j * parities).real)
+            sz_projs, spin_parities = symm.getZnProjectors(sz_flip_op_sub, 2)
+            spin_parities = np.round(np.exp(1j * spin_parities).real)
 
-            for pp, parity in zip(sz_projs, parities):
-                proj_full = pp * tp * p
-                if proj_full.shape[0] != 0:
-                    projs.append(proj_full)
-                    mzs_all.append(mz)
-                    kxs_all.append(kx)
-                    parity_all.append(parity)
+            for pp, parity in zip(sz_projs, spin_parities):
+                if (pp * tp * p).shape[0] != 0:
+
+                    if kx == 0:
+                        inv_op_sub = pp * tp * p * inv_op * \
+                                     p.conj().transpose() * tp.conj().transpose() * pp.conj().transpose()
+                        inv_projs, space_parities = symm.getZnProjectors(inv_op_sub, 2)
+                        space_parities = np.round(np.exp(1j * space_parities).real)
+
+                        for pps, parity_s in zip(inv_projs, space_parities):
+                            proj_full = pps * pp * tp * p
+                            if proj_full.shape[0] != 0:
+                                projs.append(proj_full)
+                                mzs_all.append(mz)
+                                kxs_all.append(kx)
+                                spin_parity_all.append(parity)
+                                space_parity_all.append(parity_s)
+                    else:
+                        projs.append(pp * tp * p)
+                        mzs_all.append(mz)
+                        kxs_all.append(kx)
+                        spin_parity_all.append(parity)
+                        space_parity_all.append(0)
 
     else:
         for tp, kx in zip(tx_projs, kxs):
-            proj_full = tp * p
-            if proj_full.shape[0] != 0:
-                projs.append(proj_full)
-                mzs_all.append(mz)
-                kxs_all.append(kx)
-                parity_all.append(0)
+            if (tp*p).shape[0] != 0:
+                if kx == 0:
+                    inv_op_sub = tp * p * inv_op * p.conj().transpose() * tp.conj().transpose()
+                    inv_projs, space_parities = symm.getZnProjectors(inv_op_sub, 2)
+                    space_parities = np.round(np.exp(1j * space_parities).real)
+
+                    for pps, parity_s in zip(inv_projs, space_parities):
+                        proj_full = pps * tp * p
+                        if proj_full.shape[0] != 0:
+                            projs.append(proj_full)
+                            mzs_all.append(mz)
+                            kxs_all.append(kx)
+                            spin_parity_all.append(0)
+                            space_parity_all.append(parity_s)
+                else:
+                    projs.append(tp * p)
+                    mzs_all.append(mz)
+                    kxs_all.append(kx)
+                    spin_parity_all.append(0)
+                    space_parity_all.append(0)
 
 # create hamiltonian
 ham = ss.createH(print_results=True)

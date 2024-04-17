bf252ceb3e10bd054f37de628bcd40f7748c8728
Peter Brown <ptbrown1729@gmail.com>
2024-04-17 00:12:28 -0700

    increase minimum supported python version to 3.9 and increment version

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
+from exact_diag import __version__
 
 setup(
     name="exact_diag",
-    version="0.0.1",
+    version=__version__,
     description="A package for doing exact-diagonalization of Fermion and spin lattice models",
     long_description="",
     author='Peter T. Brown, qi2lab',
     author_email='ptbrown1729@gmail.com',
     packages=find_packages(include=['exact_diag', 'exact_diag.*']),
-    python_requires='>=3.8',
-    install_requires=["numpy", "scipy"])
+    python_requires='>=3.9',
+    install_requires=["numpy",
+                      "scipy"],
+    extras_require={"display": ["matplotlib"]}
+)
